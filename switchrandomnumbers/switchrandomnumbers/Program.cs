1f6d38c51ca2c54455a0a2dcfbd70ca8686d371a
Opilane TTHK <opilane@tthk.ee>
2025-10-16 11:11:28 +0300

    5

---
+++ switchrandomnumbers/switchrandomnumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace switchrandomnumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("random number");
+            int number = new Random().Next(1, 7);
+             
+            switch (number)
+            {
+                case 1:
+                Console.WriteLine("number 1");
+                break;
+                case 2:
+                    Console.WriteLine("number 2");
+                    break;
+                case 3:
+                    Console.WriteLine("number 3");
+                    break;
+                case 4:
+                    Console.WriteLine("number 4");
+                    break;
+                case 5:
+                    Console.WriteLine("nuber 5");
+                    break;
+                case 6:
+                    Console.WriteLine("number 6");
+                    break;
+                    default:
+                    Console.WriteLine("error");
+                    break; 
+
+            }
+
+        }
+    }
+}
