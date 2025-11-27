c31b102a72bd168710b53cfa2edf52ff9ac876bd
Opilane TTHK <opilane@tthk.ee>
2025-11-27 11:11:52 +0200

    *

---
+++ SulgudeKasutamine/SulgudeKasutamine/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SulgudeKasutamine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sulgude kasutamine");
+            Console.WriteLine("Kui palju on 3+8/(4-2) *4 = ?");
+
+            double calculation1 = (3 + 8) / (4 - 2) * 4;
+            Console.WriteLine("Vastus " + calculation1);
+        }
+    }
+}
