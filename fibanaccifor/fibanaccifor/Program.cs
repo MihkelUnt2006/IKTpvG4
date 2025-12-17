f0c777da0e4b25ca0e29c71348f503a8f5909c75
Opilane TTHK <opilane@tthk.ee>
2025-12-17 14:17:14 +0200

    wedawsd

---
+++ fibanaccifor/fibanaccifor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace fibanaccifor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci");
+            int prv = 0, pre = 1, trm, i, n;
+            Console.WriteLine("sisesta number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Siin ob Fibonacci seeria kuni {0}: \n", n);
+            Console.WriteLine($"{0}  {1}", prv, pre);
+            for (i  = 0; i <= n; i++)
+            {
+                trm = prv + pre;
+                Console.Write("{0} ", trm);
+                prv = pre;
+                pre = prv;
+            }
+        }
+    }
+}
