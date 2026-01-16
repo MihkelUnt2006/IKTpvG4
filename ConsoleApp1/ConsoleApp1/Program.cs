6c8f6e677751dcea55971f79f4ea9fe7b15fe8ea
mihkel <mihunt29@gmail.com>
2026-01-16 15:04:47 +0200

    file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta faili läbi konsooli");
+            string filePath = @"C:\Users\opilane\Desktop/Uus Tekstidokument (2).txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filePath, inputText);
+        }
+        
+        
+
+       
+    }
+}
